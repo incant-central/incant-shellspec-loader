68065be2f4ce4c049cea58a87dea2f8bc335ce46
Mac Heller-Ogden <mac@machellerogden.com>
2019-10-31 13:35:47 -0500

    stream support

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { spawn } = require('child_process');
 function ShellLoader({ spec }) {
     const loader = ShellSpec(spec);
 
-    async function shellTarget(config = {}, { name:cmdPath }) { // TODO: second arg is currently, `onCancel` ... needs to pass reflection context
+    async function shellTarget(config = {}, { Resource:cmdPath }) {
         cmdPath = Array.isArray(cmdPath)
             ? cmdPath.slice(1)
             : cmdPath.split('.').slice(1);
