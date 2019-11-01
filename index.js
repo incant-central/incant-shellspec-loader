d4342dfbcf340b30c25d865486fb8e587aab5035
Mac Heller-Ogden <mac@machellerogden.com>
2019-10-31 22:34:48 -0500

    add cancellation support

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ const { spawn } = require('child_process');
 function ShellLoader({ spec }) {
     const loader = ShellSpec(spec);
 
-    async function shellTarget(config = {}, { Resource:cmdPath }) {
+    async function shellTarget(config = {}, { Resource:cmdPath, onCancel }) {
         cmdPath = Array.isArray(cmdPath)
             ? cmdPath.slice(1)
             : cmdPath.split('.').slice(1);
         const [ command, ...args ] = await loader.promptedArgv(config, cmdPath);
-        return spawn(command, args);
+        const spawned = spawn(command, args, { stdio: 'pipe', maxBuffer: 100000000 });
+        onCancel(() => spawned.kill());
+        return spawned;
     }
 
     return shellTarget;
